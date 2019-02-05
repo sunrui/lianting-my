a565bad4b3450f9de7a5527c39d7fa56296880ba
孙瑞 <>
2019-02-05 17:30:13 +0800

    改名错误

--- components/b/BOrderOne.vue
+++ components/b/BOrderOne.vue
@@ -37,9 +37,9 @@
           <div class="order_food_price order_food_price_2">{{orderFood.count * orderFood.foodPrice}}</div>
           <div class="order_food_button order_food_button_return" v-if="role === 'admin'" @click="btnFoodReturn(orderFood)">退菜</div>
           <div v-else>
-            <div class="order_food_button order_food_button_finish" v-if="orderFood.status === 'Cooked'" @click="btnStatus(orderFood, 'Finish')">上菜</div>
-            <div class="order_food_button order_food_button_cooking" v-if="orderFood.status === 'Wait'" @click="btnStatus(orderFood, 'Cooking')">开始做</div>
-            <div class="order_food_button order_food_button_cooked" v-if="orderFood.status === 'Cooking'" @click="btnStatus(orderFood, 'Cooked')">做好了</div>
+            <div class="order_food_button order_food_button_finish" v-if="orderFood.status === 'Cooked'" @click="btnChangeStatus(orderFood, 'Finish')">上菜</div>
+            <div class="order_food_button order_food_button_cooking" v-if="orderFood.status === 'Wait'" @click="btnChangeStatus(orderFood, 'Cooking')">开始做</div>
+            <div class="order_food_button order_food_button_cooked" v-if="orderFood.status === 'Cooking'" @click="btnChangeStatus(orderFood, 'Cooked')">做好了</div>
           </div>
         </div>
 
