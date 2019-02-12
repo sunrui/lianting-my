4248869e5f5e20e2b3038f902a8e2218bdc0dfa8
孙瑞 <>
2019-02-12 17:11:38 +0800

    优惠券列表，合并已失效红包。

--- pages/m/_shortId/coupon/history/index.vue
+++ pages/m/_shortId/coupon/history/index.vue
@@ -30,9 +30,11 @@
       </div>
     </div>
 
-    <div class="limit_coupon_label" v-if="http.res.coupon.invalid.length > 0">已过期红包 ({{http.res.coupon.invalid.length}}张)</div>
+    <div class="limit_coupon_label" v-if="http.res.coupon.expired.length > 0 || http.res.coupon.used.length > 0">
+      已失效红包 ({{http.res.coupon.expired.length + http.res.coupon.used.length}}张)
+    </div>
 
-    <div class="box" v-for="coupon in http.res.coupon.invalid">
+    <div class="box" v-for="coupon in http.res.coupon.expired">
       <div class="coupon coupon_white box_radius_header">
         <div class="coupon_title">
           <div class="coupon_title_label coupon_disable">优惠券</div>
@@ -58,7 +60,45 @@
           </div>
         </div>
       </div>
+      <div class="coupon_limit box_radius_footer">
+        <div class="coupon_limit_title">已失效原因</div>
+        <div class="coupon_limit_label">· 已过期</div>
+      </div>
     </div>
+
+    <div class="box" v-for="coupon in http.res.coupon.used">
+      <div class="coupon coupon_white box_radius_header">
+        <div class="coupon_title">
+          <div class="coupon_title_label coupon_disable">优惠券</div>
+          <div class="coupon_title_charge">
+            <div class="coupon_title_charge_icon coupon_title_charge_icon_disable">{{
+              coupon.type === 'FREE' ? '无门槛优惠券' :
+              coupon.type === 'NEW_USER' ? '新用户' :
+              coupon.type === 'ORDER_RETURN' ? '消费满返赠' : coupon.type
+              }}
+            </div>
+            <div class="coupon_title_charge_label coupon_disable">满{{coupon.chargePrice}}元可用</div>
+          </div>
+        </div>
+        <div class="coupon_price coupon_price_disable">{{coupon.deductPrice}}</div>
+
+        <div class="box_divide"></div>
+
+        <div class="coupon_footer">
+          <div class="coupon_expired_time">有效期：{{
+            new Date(parseInt(coupon.validAt)).toLocaleDateString() }} 至 {{
+            new Date(parseInt(coupon.expiredAt)).toLocaleDateString()
+            }}
+          </div>
+        </div>
+      </div>
+      <div class="coupon_limit box_radius_footer">
+        <div class="coupon_limit_title">不可用原因</div>
+        <div class="coupon_limit_label">· 已使用</div>
+      </div>
+    </div>
+
+    <div class="blank_30"></div>
   </section>
 </template>
 
@@ -85,7 +125,8 @@
           res: {
             coupon: {
               valid: [],
-              invalid: []
+              expired: [],
+              used: []
             }
           }
         },
@@ -108,14 +149,16 @@
             let coupon = res.elements[index]
 
             if (Boolean(coupon.orderId)) {
-              this.http.res.coupon.invalid.push(coupon)
-            } else {
-              if (new Date().getTime() > coupon.expiredAt) {
-                this.http.res.coupon.invalid.push(coupon)
-              } else {
-                this.http.res.coupon.valid.push(coupon)
-              }
+              this.http.res.coupon.used.push(coupon)
+              continue
             }
+
+            if (new Date().getTime() > coupon.expiredAt) {
+              this.http.res.coupon.expired.push(coupon)
+              continue
+            }
+
+            this.http.res.coupon.valid.push(coupon)
           }
         })
       }
