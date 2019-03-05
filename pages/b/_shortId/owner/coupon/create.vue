4275231b7e601bdaa5dccdeb1b8156312b8d3433
孙瑞 <>
2019-03-06 01:40:40 +0800

    添加餐桌框和餐桌框一样。领取次数文案修改。

--- pages/b/_shortId/owner/coupon/create.vue
+++ pages/b/_shortId/owner/coupon/create.vue
@@ -65,7 +65,7 @@
         <div class="box_divide" v-if="http.req.coupon.type === 'FREE'"></div>
 
         <div class="addition_item" v-if="http.req.coupon.type === 'FREE'">
-          <div class="addition_item_label">限制次数</div>
+          <div class="addition_item_label">领取次数</div>
           <input class="addition_item_input" placeholder="请输入次数" maxlength="3"
                  oninput="this.value=this.value.replace(/[^0-9]/g,'');"
                  v-model="http.req.coupon.perLimit">
