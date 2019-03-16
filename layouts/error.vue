70a8f2fb9ebf330f586cd0ca8833a337858d31ce
孙瑞 <>
2019-03-16 12:23:30 +0800

    出错页不再有标题。

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <title-bar :can-back="title.canBack" :title="title.title" :back-uri="title.backUri" :theme="title.theme" :imageHeight="title.imageHeight"></title-bar>
-
     <div class="empty">
       <img class="empty_image" src="/img/no/no_crash.png" :alt="ui.content">
       <div class="empty_label" v-if="error.statusCode === 404">{{ui.content}}</div>
@@ -21,13 +19,6 @@
     components: { TitleBar },
     data() {
       return {
-        title: {
-          canBack: false,
-          title: '内部错误',
-          backUri: null,
-          theme: 'white',
-          imageHeight: 0
-        },
         ui: {
           content: '404 - 呃〜好像走丢了！~'
         }
