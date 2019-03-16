5e0eaba9f4f01995bc4c15b97e0debb4037a63d6
孙瑞 <>
2019-03-16 14:40:26 +0800

    将 vue 相关的依赖显式加入，新版本 npm 会检测到不一致。 没有角色时，title-bar 隐藏，但是仍然控制不可后退。

--- pages/shop/empty.vue
+++ pages/shop/empty.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <title-bar class="title_bar_hide" :can-back="title.canBack" :title="title.title" :back-uri="title.backUri" :theme="title.theme" :imageHeight="title.imageHeight"></title-bar>
     <empty image="/img/no/no_role_user.png" content="您尚未成为任何店铺的内部人员。"></empty>
     <div class="no_role_user_tip">您可以联系管理员为您分配店铺权限。</div>
   </div>
@@ -14,11 +15,25 @@
       title: '没有角色'
     },
     middleware: 'auth',
-    components: {Empty}
+    components: {TitleBar, Empty},
+    data() {
+      return {
+        title: {
+          canBack: false,
+          title: '没有角色',
+          theme: 'white',
+          imageHeight: 0
+        },
+      }
+    }
   }
 </script>
 
 <style scoped lang="scss">
+  .title_bar_hide {
+    visibility: hidden;
+  }
+
   .no_role_user_tip {
     position: fixed;
     bottom: px2rem(30);
