398f013178d324ca18e6fd0946cee10dacc00b61
孙瑞 <>
2019-03-17 18:27:09 +0800

    登录页相关样式修改。

--- plugins/https.js
+++ plugins/https.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
-  let url = window.location.href
   if (document.location.protocol === 'http:') {
-    url = url.replace('http:', 'https:')
+    let url = window.location.href.replace('http:', 'https:')
     window.location.replace(url)
   }
 }
