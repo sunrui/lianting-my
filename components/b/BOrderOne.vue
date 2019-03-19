0f9488ce9df62f7bf468fb5cdef5ecfdb4a7c94f
孙瑞 <>
2019-03-19 14:59:15 +0800

    加入对orderFood状态已取消的显示。

--- components/b/BOrderOne.vue
+++ components/b/BOrderOne.vue
@@ -40,6 +40,7 @@
             <div class="order_food_button order_food_button_finish" v-if="orderFood.status === 'Cooked'" @click="btnChangeStatus(orderFood, 'Finish')">上菜</div>
             <div class="order_food_button order_food_button_cooking" v-if="orderFood.status === 'Wait'" @click="btnChangeStatus(orderFood, 'Cooking')">开始做</div>
             <div class="order_food_button order_food_button_cooked" v-if="orderFood.status === 'Cooking'" @click="btnChangeStatus(orderFood, 'Cooked')">做好了</div>
+            <div class="order_food_button button_gray" v-if="orderFood.status === 'Cancel'">已取消</div>
           </div>
         </div>
 
