686608c379e5f8ef52f708d6655a0e20729c0de8
孙瑞 <honeysense@outlook.com>
2019-04-09 17:44:01 +0800

    加入判断暂无收益。

--- pages/b/_shortId/owner/charge/index.vue
+++ pages/b/_shortId/owner/charge/index.vue
@@ -9,25 +9,27 @@
           <div class="blank_40"></div>
 
           <div class="charge_body_title">
-            <div class="charge_body_title_one">统计日期</div>
+            <div class="charge_body_title_one">交易日期</div>
             <div class="charge_body_title_one">成交订单</div>
             <div class="charge_body_title_one">就餐人数</div>
-            <div class="charge_body_title_one">总计收益</div>
+            <div class="charge_body_title_one">总计金额</div>
           </div>
 
           <div class="box_divide"></div>
 
-          <div v-for="statOrder in http.res.statOrder.elements">
-            <div class="charge_body_content">
-              <div class="charge_body_content_one">{{getDate(statOrder.date)}}</div>
-              <div class="charge_body_content_one">{{statOrder.totalOrder}}</div>
-              <div class="charge_body_content_one">{{statOrder.totalPeople}}</div>
-              <div class="charge_body_content_one charge_body_content_price">{{statOrder.totalRevenue}}</div>
+          <div v-if="http.res.statOrder.elements.length > 0">
+            <div v-for="statOrder in http.res.statOrder.elements">
+              <div class="charge_body_content">
+                <div class="charge_body_content_one">{{getDate(statOrder.date)}}</div>
+                <div class="charge_body_content_one">{{statOrder.totalOrder}}</div>
+                <div class="charge_body_content_one">{{statOrder.totalPeople}}</div>
+                <div class="charge_body_content_one charge_body_content_price">{{statOrder.totalRevenue}}</div>
+              </div>
+              <div class="box_divide" v-if="statOrder !== http.res.statOrder.elements[http.res.statOrder.elements.length - 1]"></div>
             </div>
-            <div class="box_divide" v-if="statOrder !== http.res.statOrder.elements[http.res.statOrder.elements.length - 1]"></div>
+            <div class="charge_footer"></div>
           </div>
-
-          <div class="blank_20"></div>
+          <div class="charge_empty" v-else>暂时没有收益。</div>
         </div>
       </div>
     </div>
@@ -53,8 +55,8 @@
           canBack: true,
           title: '收益',
           backUri: `/b/${this.$route.params.shortId}/owner`,
-          theme: 'white',
-          imageHeight: 0
+          theme: 'image',
+          imageHeight: 300
         },
         http: {
           res: {
