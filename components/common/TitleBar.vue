4a89d3a252258ebe304058306d12170500e57ade
sun rui <smallrui@126.com>
2019-04-18 08:54:31 +0800

    在关闭微信时加入试图 back。

--- components/common/TitleBar.vue
+++ components/common/TitleBar.vue
@@ -152,7 +152,7 @@
           this.$router.push(this.ui.backUri)
         } else {
           wechatApi.closeWindow()
-          history.back()
+          window.history.back()
         }
       }
     }
