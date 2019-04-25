6213997c973dcf63ab05ed54caf58cb749da067d
sun rui <smallrui@126.com>
2019-04-25 10:46:44 +0800

    修正 bd.js 错误。

--- plugins/bd.js
+++ plugins/bd.js
@@ -1,4 +1,5 @@
-var _hmt = _hmt || []
+var _hmt = _hmt || [];
+
 (function () {
   var hm = document.createElement('script')
   hm.src = 'https://hm.baidu.com/hm.js?9e87145985616d2046873dce233ecd64'
