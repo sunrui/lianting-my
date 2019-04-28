46e567aceeac78caa2d49303bd309dc342eb2ee4
sun rui <smallrui@126.com>
2019-04-28 11:01:48 +0800

    统计 js 文件更名。暂时只有百度统计，没有 360 统计。

--- plugins/stat.js
+++ plugins/stat.js
@@ -1,8 +1,7 @@
 var _hmt = _hmt || [];
-
 (function () {
   var hm = document.createElement('script')
-  hm.src = 'https://hm.baidu.com/hm.js?5052755892296faa83935a0eb585ee35'
+  hm.src = 'https://hm.baidu.com/hm.js?9e87145985616d2046873dce233ecd64'
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(hm, s)
 })()
