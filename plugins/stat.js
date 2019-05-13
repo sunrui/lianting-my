7ba04ce6410adf180241458a9263b4b0edf4fa32
sun rui <smallrui@126.com>
2019-05-13 16:51:13 +0800

    加入 360 统计

--- plugins/stat.js
+++ plugins/stat.js
@@ -4,4 +4,17 @@ var _hmt = _hmt || [];
   hm.src = 'https://hm.baidu.com/hm.js?5052755892296faa83935a0eb585ee35'
   var s = document.getElementsByTagName('script')[0]
   s.parentNode.insertBefore(hm, s)
-})()
+})();
+
+(function (b, a, e, h, f, c, g, s) {
+  b[h] = b[h] || function () {
+    (b[h].c = b[h].c || []).push(arguments)
+  }
+  b[h].s = !!c
+  g = a.getElementsByTagName(e)[0]
+  s = a.createElement(e)
+  s.src = '//s.union.360.cn/' + f + '.js'
+  s.defer = !0
+  s.async = !0
+  g.parentNode.insertBefore(s, g)
+})(window, document, 'script', '_qha', 307528, false)
