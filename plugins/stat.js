f6d4f482030023edc38c34e1cdd8f7ddd6880f27
sun rui <smallrui@126.com>
2019-05-17 14:24:12 +0800

    更新百度统计号。

--- plugins/stat.js
+++ plugins/stat.js
@@ -1,9 +1,9 @@
 var _hmt = _hmt || [];
-(function () {
-  var hm = document.createElement('script')
-  hm.src = 'https://hm.baidu.com/hm.js?5052755892296faa83935a0eb585ee35'
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(hm, s)
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e72547dde69d572853f77f7b84bf32cc";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
 })();
 
 (function (b, a, e, h, f, c, g, s) {
