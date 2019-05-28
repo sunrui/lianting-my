cdb316579de795e2cd5fa70a1270f02a9572cd47
sun rui <smallrui@126.com>
2019-05-28 11:15:14 +0800

    将续费其它年承现出来。

--- pages/b/_shortId/owner/license/index.vue
+++ pages/b/_shortId/owner/license/index.vue
@@ -380,6 +380,18 @@
           <div class="charge_year_tip">限时特惠</div>
         </div>
 
+        <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 2}"
+             @click="btnChooseYear(2)">续费 2 年
+        </div>
+
+        <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 3}"
+             @click="btnChooseYear(3)">续费 3 年
+        </div>
+
+        <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 5}"
+             @click="btnChooseYear(5)">续费 5 年
+        </div>
+
         <div class="modal_button_box">
           <div class="button_big" @click="btnChargeConfirm">立即续费</div>
         </div>
