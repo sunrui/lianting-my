24b7670739e7278b4b66f288f3b19e34952f3aba
sun rui <smallrui@126.com>
2019-08-05 23:46:56 +0800

    默认使用 webp 显示。

--- api/local/imageApi.js
+++ api/local/imageApi.js
@@ -1,5 +1,5 @@
 export const imageApi = {
   getXOssProcess() {
-    return '?x-oss-process=image/auto-orient,1/interlace,1/resize,m_fill,w_600,h_600/quality,q_85/format,jpg'
+    return '?x-oss-process=image/auto-orient,1/resize,m_fill,w_800,h_800/quality,q_85/format,webp'
   }
 }
