ea83a305e983ecd6596ab25a263adeafa479d741
sun rui <smallrui@126.com>
2019-08-28 02:42:56 +0800

    加入会员储值卡旗舰会员专享。 回归充值优惠。

--- pages/b/_shortId/owner/license/index.vue
+++ pages/b/_shortId/owner/license/index.vue
@@ -321,6 +321,17 @@
             <div class="box_divide"></div>
           </div>
 
+          <div v-if="license.plan.licenseType === 'Senior'">
+            <div class="shop_feature_one">
+              <img class="shop_feature_one_icon" :src="
+          license.plan.licenseType === 'Senior' ? '/img/b/license/b_license_senior_check.png' : '/img/b/license/b_license_free_check.png'
+          " alt="">
+              <div class="shop_feature_one_label">会员储值卡<span class="shop_feature_one_label_star addition_item_tag_color_1">专享</span></div>
+            </div>
+
+            <div class="box_divide"></div>
+          </div>
+
           <div class="shop_feature_one">
             <img class="shop_feature_one_icon" :src="
             license.plan.licenseType === 'Free' ? '/img/b/license/b_license_free_check.png' :
@@ -433,15 +444,18 @@
         </div>
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 2}"
-             @click="btnChooseYear(2)">续费 2 年 <div class="charge_year_tip">限时送 1 年</div>
+             @click="btnChooseYear(2)">续费 2 年
+          <div class="charge_year_tip">限时送 1 年</div>
         </div>
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 3}"
-             @click="btnChooseYear(3)">续费 3 年 <div class="charge_year_tip">限时送 2 年</div>
+             @click="btnChooseYear(3)">续费 3 年
+          <div class="charge_year_tip">限时送 2 年</div>
         </div>
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 5}"
-             @click="btnChooseYear(5)">续费 5 年 <div class="charge_year_tip">限时送 5 年</div>
+             @click="btnChooseYear(5)">续费 5 年
+          <div class="charge_year_tip">限时送 5 年</div>
         </div>
 
         <div class="blank_20"></div>
