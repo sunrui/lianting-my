ef97bb33f0df06ac1ce7d4a0c29c3641f9ac42ea
sun rui <smallrui@126.com>
2019-10-09 22:48:53 +0800

    价格修改，从 890 2890 4890 更为 890 1890 2890.

--- pages/b/_shortId/owner/license/index.vue
+++ pages/b/_shortId/owner/license/index.vue
@@ -426,22 +426,19 @@
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 1}"
              @click="btnChooseYear(1)">续费 1 年 (永久使用)
-          <div class="charge_year_tip">限时活动</div>
+          <div class="charge_year_tip">来年免费续费</div>
         </div>
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 2}"
              @click="btnChooseYear(2)">续费 2 年
-          <div class="charge_year_tip">赠送 1 年</div>
         </div>
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 3}"
              @click="btnChooseYear(3)">续费 3 年
-          <div class="charge_year_tip">赠送 2 年</div>
         </div>
 
         <div class="modal_menu" v-bind:class="{modal_menu_select: ui.year === 5}"
              @click="btnChooseYear(5)">续费 5 年
-          <div class="charge_year_tip">赠送 5 年</div>
         </div>
 
         <div class="modal_button_box">
