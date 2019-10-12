ae8d08c89ce2c039cd36ad25e8e5b451cc9657ad
sun rui <smallrui@126.com>
2019-10-12 15:32:08 +0800

    打印机下载域名打错。

--- pages/b/_shortId/owner/printer/create/yly.vue
+++ pages/b/_shortId/owner/printer/create/yly.vue
@@ -5,7 +5,7 @@
     <div class="box">
       <div class="tip">
         <ul class="tip_ul">
-          <li>易联云<a class="tip_link" href="https://www.yilianyun.net">官网</a>接入文档请<a class="tip_link" href="https://www.honseysense.com/doc/printer_yly.docx">下载</a>。</li>
+          <li>易联云<a class="tip_link" href="https://www.yilianyun.net">官网</a>接入文档请<a class="tip_link" href="https://www.honeysense.com/doc/printer_yly.docx">下载</a>。</li>
         </ul>
       </div>
     </div>
--- pages/b/_shortId/owner/printer/create/zhongwu.vue
+++ pages/b/_shortId/owner/printer/create/zhongwu.vue
@@ -5,7 +5,7 @@
     <div class="box">
       <div class="tip">
         <ul class="tip_ul">
-          <li>中午云<a class="tip_link" href="http://www.zhongwu.co/">官网</a>接入文档请<a class="tip_link" href="https://www.honseysense.com/doc/printer_zhongwu.docx">下载</a>。</li>
+          <li>中午云<a class="tip_link" href="http://www.zhongwu.co/">官网</a>接入文档请<a class="tip_link" href="https://www.honeysense.com/doc/printer_zhongwu.docx">下载</a>。</li>
         </ul>
       </div>
     </div>
