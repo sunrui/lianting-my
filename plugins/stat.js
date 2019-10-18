0e41ac4fe1dc1502aa83d6ce76c988899dceec73
sun rui <smallrui@126.com>
2019-10-18 11:22:39 +0800

    更换百度统计 shop.lt.city 的 id 到 shop.honeysense.com

--- plugins/stat.js
+++ plugins/stat.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'development') {
   let _hmt = _hmt || [];
   (function () {
     let hm = document.createElement('script')
-    hm.src = 'https://hm.baidu.com/hm.js?a919c3274d8c4eca1d261a8dac6d2776'
+    hm.src = 'https://hm.baidu.com/hm.js?2e42d950e67ef32101d4d3bb243dd062'
     let s = document.getElementsByTagName('script')[0]
     s.parentNode.insertBefore(hm, s)
   })()
