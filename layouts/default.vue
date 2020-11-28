525d1937d19c3fe33aedf704765ebf25c5c36489
sunrui <smallrui@foxmail.com>
2020-11-28 17:20:42 +0800

    不刷新 bug

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="container" v-show="false" id="nuxt_div" v-lazy-container="{ selector: 'img' }">
-    <nuxt/>
-    <loading v-show="false" id="nuxt_loading"></loading>
+  <div class="container" v-show="false" id="nuxt_div">
+    <nuxt v-lazy-container="{ selector: 'img' }"/>
+    <loading v-show="false" id="nuxt_loading" v-lazy-container="{ selector: 'img' }"></loading>
   </div>
 </template>
 
 <script>
-  import Loading from '../components/common/Loading'
-  import {remApi} from '../api/local/remApi'
+import Loading from '../components/common/Loading'
+import {remApi} from '@/api/local/remApi'
 
-  export default {
-    name: 'default',
-    components: {Loading},
-    mounted() {
-      remApi.init()
-    }
+export default {
+  name: 'default',
+  components: {Loading},
+  mounted() {
+    remApi.init()
   }
+}
 </script>
 
 <style scoped lang="scss">
-  html, body {
-    font-size: 26px;
-    margin: 0;
-    padding: 0;
-  }
+html, body {
+  font-size: 26px;
+  margin: 0;
+  padding: 0;
+}
 
-  .container {
-    font-size: 26px;
-    margin: 0;
-    padding: 0;
-  }
+.container {
+  font-size: 26px;
+  margin: 0;
+  padding: 0;
+}
 
-  *,
-  *:before,
-  *:after {
-    box-sizing: border-box;
-    font-size: 26px;
-    margin: 0;
-    padding: 0;
-  }
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  font-size: 26px;
+  margin: 0;
+  padding: 0;
+}
 </style>
