a45777f1d105917f652a2a3ab71cdee3ab0143be
sunrui <honeysense@outlook.com>
2023-06-10 19:53:31 +0800

    1、更新了布署文件。

--- doc/pm2_build.sh
+++ doc/pm2_build.sh
@@ -1,23 +1,20 @@
 pm2 delete all
-cd lianting-www
+cd ~/lianting-www
 rm -rf package-lock.json
 git pull
 npm install
 npm run build
 pm2 start npm --name "lianting-www" -- run start --watch
-cd ..
-cd lianting-m
+cd ~/lianting-m
 rm -rf package-lock.json
 git pull
 npm install
 npm run build
 pm2 start npm --name "lianting-m" -- run start --watch
-cd ..
-cd lianting-my
+cd ~/lianting-my
 rm -rf package-lock.json
 git pull
 npm install
 npm run build
 pm2 start npm --name "lianting-my" -- run start --watch
-cd ..
 ~
