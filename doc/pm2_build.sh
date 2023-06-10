94b29bbd842dc3e567fdf5b62ec2dc6e9e729e82
sunrui <honeysense@outlook.com>
2023-06-10 15:25:00 +0800

    1、更新 b.lianting.store 为 my.lianting.store。

--- doc/pm2_build.sh
+++ doc/pm2_build.sh
@@ -13,11 +13,11 @@ npm install
 npm run build
 pm2 start npm --name "lianting-m" -- run start --watch
 cd ..
-cd lianting-b
+cd lianting-my
 rm -rf package-lock.json
 git pull
 npm install
 npm run build
-pm2 start npm --name "lianting-b" -- run start --watch
+pm2 start npm --name "lianting-my" -- run start --watch
 cd ..
 ~
