33489ea17267fd2f66aed79dcc6b82411ddf0c56
ruisun <24170832@qq.com>
2023-12-20 09:39:30 +0800

    1、更新了百度统计《上海唏美》账号的打点。

--- plugins/stat.js
+++ plugins/stat.js
@@ -1,9 +1,9 @@
 if (process.env.NODE_ENV !== 'development') {
-  let _hmt = _hmt || [];
-  (function () {
-    let hm = document.createElement('script')
-    hm.src = 'https://hm.baidu.com/hm.js?a919c3274d8c4eca1d261a8dac6d2776'
-    let s = document.getElementsByTagName('script')[0]
-    s.parentNode.insertBefore(hm, s)
-  })()
+    let _hmt = _hmt || [];
+    (function () {
+        let hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?d4ae31b8e094c2077b549d750b6a6611";
+        let s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
 }
